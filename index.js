b08850dd66186b82e4a3ff49ab8692851ed02773
nehemiahw <nehemiah.washington6@gmail.com>
2020-07-29 09:53:11 -0500

    'added functionality to the index.js file, added scripts to package.json'

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ const helmet = require('helmet');
 
 //instantiate express
 const app = express();
+
+
+//'use' statements, declaring middlewares
+app.use(express.static(__dirname + "/public"));
+app.use(helmet());
+
+//Declare port to run on
+const PORT = process.env.PORT || 8080;
+
+//listen on the declared port
+app.listen(PORT, ()=>{
+    console.log(`listening on port: ${PORT}`);
+})
+
+
