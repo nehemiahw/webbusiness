392bc5dbe35aa9b08fc2db76bf6afe686e7764a5
nehemiahw <nehemiah.washington6@gmail.com>
2020-07-29 20:26:40 -0500

    removed header code

--- index.js
+++ index.js
@@ -10,12 +10,24 @@ const app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(helmet());
 
+//HandleBars setting
+app.set("view engine", "hbs");
+app.engine('hbs', expressHandlebars({
+    extname: 'hbs',
+    defaultLayout: 'index'
+}));
+
 //Declare port to run on
 const PORT = process.env.PORT || 8080;
 
 //listen on the declared port
 app.listen(PORT, ()=>{
     console.log(`listening on port: ${PORT}`);
+})
+
+//Landing Page
+app.get('/', (req, res)=>{
+    res.send("server Working!");
 })
 
 
