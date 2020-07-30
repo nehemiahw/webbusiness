5d7c616627661318e40e7d7a66408df984ac79d6
nehemiahw <nehemiah.washington6@gmail.com>
2020-07-29 22:07:56 -0500

    initial heroku commit

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ const app = express();
 
 //'use' statements, declaring middlewares
 app.use(express.static(__dirname + "/public"));
+app.use(express.static('views/images'));
 app.use(helmet());
 
 //HandleBars setting
 app.set("view engine", "hbs");
 app.engine('hbs', expressHandlebars({
     extname: 'hbs',
-    defaultLayout: 'index'
+    defaultLayout: 'index',
+    layoutsDir: __dirname+ '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
 }));
 
 //Declare port to run on
@@ -27,7 +30,23 @@ app.listen(PORT, ()=>{
 
 //Landing Page
 app.get('/', (req, res)=>{
-    res.send("server Working!");
+    res.render("main");
+})
+
+app.get('/about', (req, res)=>{
+    res.render("about");
+})
+
+app.get('/portfolio', (req, res)=>{
+    res.render("portfolio");
+})
+
+app.get('/contact', (req, res)=>{
+    res.render("contact");
+})
+
+app.get('*', (req, res)=>{
+    res.render("notfound");
 })
 
 
